e0c66e6600b849d788d561905df11c9deb964e45
Rodrigo Cardoso de Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2019-04-14 12:12:20 -0300

    fim da compatibilidade com o firefox

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -6,12 +6,13 @@ class DateHelper{
 
     static textoParaData( texto ){
         
-        if (!(/^\d{4}-\d{2}-\d{2}$/.test(texto)))
-            throw new Error("Deve estar no formato ano aaaa-mm-dd");
+        if (!(/\d{2}\/\d{2}\/\d{4}/.test(texto)))
+            throw new Error("Deve estar no formato ano dd/mm/yyyy");
 
         return new Date(...
                         texto
-                        .split("-")
+                        .split("/")
+                        .reverse()
                         .map( (item, indice) => item - indice % 2));
     }
 
